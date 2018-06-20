2900af62495277048de16c357b202c49fea5ba0f
DrewGaard <d_overgaard1@u.pacific.edu>
2018-06-19 18:21:42 -0700

    Recursive binary search

--- learning-algorithms/src/algo/binarysearch/App.java
+++ learning-algorithms/src/algo/binarysearch/App.java
@@ -23,9 +23,19 @@ public class App {
 		return -1;
 	}
 	
-	public static int recursiveBinarySearch(int [] a, int p, int r, int x)
-	{
-		
+	public static int recursiveBinarySearch(int [] a, int p, int r, int x) {
+		if(p > r) {
+			return -1;
+		} else {
+			int q = (p + r) / 2;
+			if(a[q] == x) {
+				return q;
+			} else if(a[q] > x) {
+				return recursiveBinarySearch(a, p, q-1, x);
+			} else {
+				return recursiveBinarySearch(a, q+1, r, x);
+			}
+		}
 	}
 
 }
