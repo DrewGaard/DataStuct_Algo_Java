cbd61afeb0cae32f8cf200281f857ad54ec7d644
DrewGaard <d_overgaard1@u.pacific.edu>
2018-06-21 10:00:58 -0700

    Completed selection sort

--- learning-algorithms/src/algo/selectionsort/App.java
+++ learning-algorithms/src/algo/selectionsort/App.java
@@ -10,7 +10,17 @@ public class App {
 	}
 	
 	public static int [] selectionSort(int a[]) {
-		
+		for(int i = 0; i < a.length - 1; i++) {
+			int minimum = i;
+			for(int j = i + 1; j < a.length; j++) {
+				if(a[j] < a[minimum]) { // if we find a smaller value
+					minimum = j;
+				}
+			}
+			int temp = a[i];
+			a[i] = a[minimum];
+			a[minimum] = temp;
+		}
+		return a;
 	}
-
 }
