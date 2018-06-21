e8a813bbb7e61a3be27e4d664329ac476b12f773
DrewGaard <d_overgaard1@u.pacific.edu>
2018-06-20 22:15:33 -0700

    Selection sort start

---
+++ learning-algorithms/src/algo/selectionsort/App.java
@@ -0,0 +1,16 @@
+package algo.selectionsort;
+
+public class App {
+
+	public static void main(String[] args) {
+		int [] myArray = selectionSort(new int[] {9,8,3,13,87,12,99});
+		for(int i=0; i < myArray.length; i++) {
+			System.out.println(myArray[i]);
+		}
+	}
+	
+	public static int [] selectionSort(int a[]) {
+		
+	}
+
+}
